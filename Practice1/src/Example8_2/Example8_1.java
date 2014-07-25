a4aa1a4781edf85a371ac32ece56a499f226c967
AlicJ <zichen.jiang@yahoo.com>
2014-07-25 12:04:40 -0400

    gitignore added, and a new practice

---
+++ Practice1/src/Example8_2/Example8_1.java
@@ -0,0 +1,63 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package Example8_2;
+
+/**
+ *
+ * @author Alic Jiang
+ */
+/*
+class Primary {
+    private String name = null;
+    
+    public Primary () {
+        name = "---";
+    }
+    
+    public Primary (String name) {
+        this.name = name;
+    }
+    
+    public Primary (char name[]) {
+        this.name = new String(name);
+    }
+    
+    public Primary (Primary stu) {
+        this.name = stu.name;
+    }
+    
+    
+    public void showName() {
+        if(name.equals("---")){
+            System.out.println("This student has no name.");
+        }else{
+            System.out.println("The name of this primary studnet is " + name + ".");
+        }
+    }
+}
+public class Example8_1 {
+    
+    public static void main(String[] argv) {
+        
+        char name[] = {'L','a','r','r','y',' ','P','a','g','e'};
+        
+        Primary student1 = new Primary("Mark Zuckerberg");
+        Primary student2 = new Primary("Sergey Brin");
+        Primary student3 = new Primary(name);
+        Primary student4 = new Primary();
+        Primary student5 = new Primary(student1);        
+        student1.showName();
+        student2.showName();
+        student3.showName();
+        student4.showName();
+        student5.showName();
+        
+        
+    }
+}
+
+*/
