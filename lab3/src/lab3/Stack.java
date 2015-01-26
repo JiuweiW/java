e82b2a70b246b7e004f4aeba1b3253c81b567870
unknown <jiangz26@BSB244-VDI-11.ads.mcmaster.ca>
2015-01-26 11:15:50 -0500

    finish lab3

---
+++ lab3/src/lab3/Stack.java
@@ -0,0 +1,16 @@
+package lab3;
+
+public class Stack {
+	Stack head;
+	Stack tail;
+	Stack prev;
+	Stack next;
+	Stack val;
+	
+	
+	
+	public static void main(String[] args) {
+		System.out.println("BLAH!");
+	}
+
+}
--- lab3/src/lab3/TestRunner.java
+++ lab3/src/lab3/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.notification.Failure;
 
 public class TestRunner {
 	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(MoneyTest.class);
+		Result result = JUnitCore.runClasses(CounterTest.class);
 		for (Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
