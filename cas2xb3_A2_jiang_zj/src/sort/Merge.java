93f9678ff06d9cd827a46f8a0fa5b2d4af29039a
JiuweiW <jiuweitongxue@gmail.com>
2017-03-08 10:32:48 -0500

    Add Documentation
    Add Documentation

--- cas2xb3_A2_jiang_zj/src/sort/Merge.java
+++ cas2xb3_A2_jiang_zj/src/sort/Merge.java
@@ -13,7 +13,13 @@ public class Merge {
 		aux = new Comparable[x.length];
 		sort(x, 0, x.length - 1);
 	}
-	
+	/**
+	 * Stably merge a[lo .. mid] with a[mid+1 ..hi] using aux[lo .. hi]
+	 * @param a - array for merge
+	 * @param lo - the beginning index of array a for merge
+	 * @param mid - the middling index of array a 
+	 * @param hi - the ending index of array a for merge
+	 */
 	private static void merge(Comparable[] a, int lo, int mid, int hi) {
 		// merge a[lo..mid] with a[mid+1..hi]
 		int i = lo, j = mid+1;
@@ -30,11 +36,22 @@ public class Merge {
 				a[k] = aux[i++];
 		}
 	}
-	
+	/**
+	 * Compares two comparable a and b.
+	 * @param a - the first comparable to be compared
+	 * @param b - the second comparable to be compared
+	 * @return {@code true} if comparable a bigger than b;
+         *         {@code false} otherwise
+	 */
 	private static boolean higher(Comparable a, Comparable b) {
 		return a.compareTo(b) > 0;
 	}
-	
+	/**
+	 * Merge sort a[lo..hi] using auxiliary array aux[lo..hi]
+	 * @param a - the input array containing scores of words that need to be sorted.
+	 * @param lo - the beginning index of array a for merge
+	 * @param hi - the ending index of array a for merge
+	 */
 	private static void sort(Comparable[] a, int lo, int hi) {
 		// sort a[lo..hi]
 		if (hi <= lo)
@@ -45,7 +62,12 @@ public class Merge {
 		merge(a, lo, mid, hi);
 	}
 	
-	// check if the array is sorted
+	/**
+	 * Check if array is sorted - useful for debugging.
+	 * @param x - the array for sort
+         * @return {@code true} if array x is sorted;
+         *         {@code false} otherwise
+	 */
 	public static boolean isSorted(Word[] x) {
 		for (int i=0; i<x.length-1; i++) {
 			if ((x[i].compareTo(x[i+1]) < 0))
@@ -53,7 +75,10 @@ public class Merge {
 		}
 		return true;
 	}
-	
+	/**
+         * Tests for merge sort
+         * @param args the command-line arguments
+         */
 //    public static void main(String[] args){
 //        Comparable a[]=new Comparable[]{12,10,34,23,9,7,8,5,6};
 //        sortMerge(a);
